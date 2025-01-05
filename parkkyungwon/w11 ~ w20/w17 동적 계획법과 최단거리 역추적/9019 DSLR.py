ed8b50ae5903957ea973c30a999af8d01375ca6b
kyungwon <power16one5@naver.com>
2025-01-05 10:30:17 +0900

    refact w17 9019 DSLR class사용과  가독성을 높힘

--- parkkyungwon/w11 ~ w20/w17 동적 계획법과 최단거리 역추적/9019 DSLR.py
+++ parkkyungwon/w11 ~ w20/w17 동적 계획법과 최단거리 역추적/9019 DSLR.py
@@ -1,72 +1,86 @@
 import sys
 
-readline = sys.stdin.readline
-L = 10000
-# 각 수에서 다른 수로 변환(정방향, 역방향 모두)할 수 있는 수를 미리 전부 구함 
-fw_path = [(i-1 if i else 9999, 10*i%L + i//1000, i//10 + 1000*i%L, 2*i%L) for i in range(L)]
-bw_path = [(0 if i == 9999 else i+1, i//10 + 1000*i%L, 10*i%L + i//1000, j := i//2, j+5000)
-            if i % 2 == 0 else 
-            (0 if i == 9999 else i+1, i//10 + 1000*i%L, 10*i%L + i//1000) for i in range(L)]
-fw_chars = ('S', 'L', 'R', 'D')
-bw_chars = ('S', 'L', 'R', 'D', 'D')
 
-# 출발 지점과 도착 지점 양쪽에서 동시에 dp를 계산해간다. 투 포인터, bfs
-def two_point_bfs(start, end):
-    fw_dp = [-1] * L
-    bw_dp = [-1] * L
-    fw_dp[start] = start
-    bw_dp[end] = end
 
-    queue1, queue2 = [start], [end]
-    dp1, dp2 = fw_dp, bw_dp
-    path1, path2 = fw_path, bw_path
+class DSLR():
+    __slots__ = ['fw_path', 'bw_path', 'fw_char', 'bw_char', 'size', 'visit', 'parent', 'command']
 
-    while 1:
-        tmp_queue = []
+    def __init__(self):
+        self.size = 10000
+        self.fw_path = [(i-1 if i else 9999, 10*i%self.size + i//1000, i//10 + 1000*i%self.size, 2*i%self.size) for i in range(self.size)]
+        self.bw_path = [(0 if i == 9999 else i+1, i//10 + 1000*i%self.size, 10*i%self.size + i//1000, j := i//2, j+5000)
+                        if i % 2 == 0 else 
+                        (0 if i == 9999 else i+1, i//10 + 1000*i%self.size, 10*i%self.size + i//1000) for i in range(self.size)]
+        self.fw_char = ('S', 'L', 'R', 'D')
+        self.bw_char = ('S', 'L', 'R', 'D', 'D')
 
-        for q in queue1:
-            for next_q in path1[q]:
-                if dp1[next_q] < 0:
-                    dp1[next_q] = q
-                    tmp_queue.append(next_q)
-                
-                    # 중간 지점을 찾으면 반환
-                    if dp2[next_q] > -1: return next_q, fw_dp, bw_dp
+    def bfs(self, start, end):
+        self.visit[start] = 1
+        self.visit[end] = 10000
+        fw_queue = [start]
+        bw_queue = [end]
+
+        for i, j in zip(range(2, 5000), range(9999, 5000, -1)):
+            tmp_queue = []
+            for u in fw_queue:
+                for v, c in zip(self.fw_path[u], self.fw_char):
+                    if self.visit[v] is None:
+                        self.visit[v] = i
+                        self.parent[v] = u
+                        self.command[v] = c
+                        tmp_queue.append(v)
+                    
+                    elif self.visit[v] > 5000: return u, v, c
+            
+            fw_queue = tmp_queue
+
+            tmp_queue = []
+            for v in bw_queue:
+                for u, c in zip(self.bw_path[v], self.bw_char):
+                    if self.visit[u] is None:
+                        self.visit[u] = j
+                        self.parent[u] = v
+                        self.command[u] = c
+                        tmp_queue.append(u)
+
+                    elif self.visit[u] < 5000: return u, v, c
+            
+            bw_queue = tmp_queue
+
+    def backtrack(self, u, v, c):
+        answer = []
+
+        while self.parent[u] is not None:
+            answer.append(self.command[u])
+            u = self.parent[u]
         
-        queue1, queue2 = queue2, tmp_queue
-        dp1, dp2 = dp2, dp1
-        path1, path2 = path2, path1
+        answer = answer[::-1]
+        answer.append(c)
 
-# 겹치는 곳에서 최단 경로를 역추적
-def back_tracking(i, dp, chars, path):
-    answer = []
+        while self.parent[v] is not None:
+            answer.append(self.command[v])
+            v = self.parent[v]
 
-    while dp[i] != i:
-        for char, j in zip(chars, path[i]):
-            if j == dp[i]:
-                answer.append(char)
-                i = j
-                break
-    
-    return answer
+        return ''.join(answer)
 
-def sol(start, end):
-    m, fw_dp, bw_dp = two_point_bfs(start, end)
-    # 최단 경로 추적
-    answer = back_tracking(m, fw_dp, bw_chars, bw_path)
-    answer.reverse() 
-    answer += back_tracking(m, bw_dp, fw_chars, fw_path)
-   
-    return ''.join(answer)
+    def sol(self, start, end):
+        self.visit = [None] * self.size
+        self.parent = [None] * self.size
+        self.command = [None] * self.size
+
+        u, v, c = self.bfs(start, end)
+        return self.backtrack(u, v, c)
+
 
 def main():
+    readline = sys.stdin.readline
     T = int(readline())
+    dslr = DSLR()
 
     for _ in range(T):
         A, B = map(int, readline().split())
 
-        sys.stdout.write(sol(A, B) + '\n')
+        sys.stdout.write(dslr.sol(A, B) + '\n')
 
 
-if __name__ == "__main__":
-    main()
+main()
